b5a680e1ef061e20b49cb552113f1951ece4e555
Aleeyeen <Aleeyeen2765@gmail.com>
2022-04-24 02:15:39 +0700

    edit_backend

--- backend/database.js
+++ backend/database.js
@@ -9,19 +9,38 @@ let users = {
 
 let bookingDeluxe ={
     list: [
-        { id: 1, name: 'Aleeyeen', surname:'Mad-E', email:'aleeyeen2765@gmail.com', tel: 0950121059, }
+        { id: 1, name: 'Aleeyeen', surname: 'Mad-E',  date: '19/04/2022', checkin: '03:30 PM', checkout: '05:30 PM'},
+        { id: 2, name: 'Harista', surname: 'Wansoo',   date: '24/04/2022', checkin: '10:30 AM', checkout: '12:30 PM'}
     ]
 }
+
+let bookingPrime ={
+    list:[
+        { id: 1, name: 'Aleeyeen', surname: 'Mad-E',  date: '19/04/2022', checkin: '03:30 PM', checkout: '05:30 PM'},
+        { id: 2, name: 'Harista', surname: 'Wansoo',   date: '24/04/2022', checkin: '10:30 AM', checkout: '12:30 PM'}
+    ]
+} 
 
 const SECRET = 'your_jwt_secret'
 const NOT_FOUND = -1
 
-exports.users = users 
+exports.users = users
+exports.bookingDeluxe = bookingDeluxe
+exports.bookingPrime = bookingPrime
+
 exports.SECRET = SECRET
 exports.NOT_FOUND = NOT_FOUND
 
 exports.setUsers = function(_users) { 
   users = _users;
+}
+
+exports.setbookingDeluxe = function(_bookingDeluxe){
+    bookingDeluxe = _bookingDeluxe
+}
+
+exports.setbookingPrime = function(_bookingPrime){
+    bookingPrime = _bookingPrime
 }
 
 // === validate username/password ===
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,16 @@
-
 const express = require('express'),
     app = express(),
     passport = require('passport'),
     port = process.env.PORT || 80,
     cors = require('cors')
+    cookie = require('cookie')
 
 const bcrypt = require('bcrypt')
 
 const db = require('./database.js')
 let users = db.users
+let bookingDeluxe = db.bookingDeluxe
+let bookingPrime = db.bookingPrime
 
 require('./passport.js')
 
@@ -27,21 +29,41 @@ router.post('/login', (req, res, next) => {
         console.log('Login: ', req.body, user, err, info)
         if (err) return next(err)
         if (user) {
-            if(req.body.rememberme == 'true'){
-                const token = jwt.sign(user, db.SECRET,{
-                    expiresIn: '7d'
+            let expi = (req.body.checktoken == 'on') ? '7d' : '1d'
+            const token = jwt.sign(user, db.SECRET, {
+                expiresIn: expi
+            })
+            // req.cookie.token = token
+            res.setHeader(
+                "Set-Cookie",
+                cookie.serialize("token", token, {
+                    httpOnly: true,
+                    secure: process.env.NODE_ENV !== "development",
+                    maxAge: 60 * 60,
+                    sameSite: "strict",
+                    path: "/",
                 })
-                return res.json({ user, token })
-            }
-            else{
-                const token = jwt.sign(user, db.SECRET,{
-                    expiresIn: '1d'
-                })
-                return res.json({ user, token })
-            } 
+            );
+            res.statusCode = 200
+            return res.json({ user, token })
         } else
             return res.status(422).json(info)
     })(req, res, next)
+})
+
+router.get('/logout', (req, res) => {
+    res.setHeader(
+        "Set-Cookie",
+        cookie.serialize("token", '', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV !== "development",
+            maxAge: -1,
+            sameSite: "strict",
+            path: "/",
+        })
+    );
+    res.statusCode = 200
+    return res.json({ message: 'Logout successful' })
 })
  
 /* GET user profile. */
@@ -69,10 +91,38 @@ router.post('/register',
         }
     })
 
+router.get('/alluser', (req, res) => res.json(db.users.users))
 
 router.get('/', (req, res, next) => {
     res.send('Respond without authentication');
 });
+
+/* -----------------------------------------------------------Booking Deluxe-------------------------------------------------------- */
+router.route('/bookingDeluxe')
+    .get((req, res) => res.send(bookingDeluxe))
+    .post((req, res) => {
+        let allnewuserbook1 = {}
+        allnewuserbook1.id = (bookingDeluxe.list.length) ? bookingDeluxe.list[bookingDeluxe.list.length - 1].id + 1 : 1
+        allnewuserbook1.name = req.body.name
+        allnewuserbook1.surname = req.body.surname
+        allnewuserbook1.email = req.body.email
+        allnewuserbook1.tel = req.body.tel
+        allnewuserbook1.date = req.body.date
+        allnewuserbook1.checkin = req.body.checkin
+        allnewuserbook1.checkout = req.body.checkout
+        bookingDeluxe = {"list": [...bookingDeluxe.list, allnewuserbook1]}
+
+        res.send(comment)
+    })
+
+router.route('/bookingDeluxe/:booking_id')
+    .get((req, res) => {
+        const bookingId = req.params.booking_id
+        const id = bookingDeluxe.list.findIndex(item => +item.id === +bookingId)
+        if (id >= 0){
+            res.send()
+        }
+    })
 
 // Error Handler
 app.use((err, req, res, next) => {

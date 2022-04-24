cf18a8ae5a9e388213a1076635ea07cf483330f8
Aleeyeen <Aleeyeen2765@gmail.com>
2022-04-25 03:30:41 +0700

    edit3

--- frontend/pages/menu.js
+++ frontend/pages/menu.js
@@ -9,14 +9,16 @@ export default function Menu({ token }) {
       <Head>
         <title>Online Karaoke Booking</title>
       </Head>
+      <div className = 'flex justify-center mt-4 text-md font-medium uppercase tracking-wider italic'>
+        " Contact : Tel 095-1430950 "</div>
       <div className = 'flex justify-center mt-14 text-4xl font-bold uppercase tracking-wider'>
         Online Karaoke Booking</div>
-      <div className = 'flex justify-center mt-4 text-md font-medium uppercase tracking-wider italic animate-pulse'>
+      <div className = 'flex justify-center mt-4 text-md font-medium uppercase tracking-wider italic'>
         " Please select type karaoke room "</div>
 
       <div className='md:flex flex-row fixed justify-evenly items-start mt-36 h-screen w-screen'>  
       <Link href='/bookingDeluxe'>
-          <a className='flex flex-col w-56 h-60 bg-sunglow shadow-lg rounded-lg transition duration-700 
+          <a className='flex flex-col w-30 h-35 transition duration-700 
             ease-in-out transform hover:-translate-y-1 hover:scale-110'>
             <div className='-m-10'>
               <img src="/booking.svg"/>
@@ -28,7 +30,7 @@ export default function Menu({ token }) {
         </Link>
      
         <Link href='/bookingPrime'>
-          <a className='flex flex-col w-56 h-60 bg-palepink shadow-lg rounded-lg transition duration-700 
+          <a className='flex flex-col w-30 h-35 transition duration-700 
             ease-in-out transform hover:-translate-y-1 hover:scale-110'>
             <div className='-m-10'>
               <img src="/booking.svg"/>
